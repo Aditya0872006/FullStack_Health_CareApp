5be9ab1707c61ff77ddf5bff4a09900af8c3c27e
Aditya Sharma <sharmaadityamj@gmail.com>
2025-12-26 10:00:01 +0530

    controller added

---
+++ src/main/java/com/example/HealthCareApp/appointment/controller/AppointmentController.java
@@ -0,0 +1,43 @@
+package com.example.HealthCareApp.appointment.controller;
+
+import com.example.HealthCareApp.appointment.dto.AppointmentDto;
+import com.example.HealthCareApp.appointment.service.AppointmentService;
+import com.example.HealthCareApp.res.Response;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/appointments")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<Response<AppointmentDto>> bookAppointment(@RequestBody @Valid AppointmentDto appointmentDTO){
+        return ResponseEntity.ok(appointmentService.bookAppointment(appointmentDTO));
+    }
+
+    @GetMapping
+    public  ResponseEntity<Response<List<AppointmentDto>>> getMyAppointments(){
+        return ResponseEntity.ok(appointmentService.getMyAppointments());
+    }
+
+    @PutMapping("/cancel/{appointmentId}")
+    public  ResponseEntity<Response<AppointmentDto>> cancelAppointment(@PathVariable Long appointmentId){
+        return ResponseEntity.ok(appointmentService.cancelAppointment(appointmentId));
+    }
+
+    @PutMapping("/complete/{appointmentId}")
+    @PreAuthorize(("hasAuthority('DOCTOR')"))
+    public  ResponseEntity<Response<?>> completeAppointment(@PathVariable Long appointmentId){
+        return ResponseEntity.ok(appointmentService.completeAppointment(appointmentId));
+    }
+
+}
