bc5b6847ab35e8ef4653ad90f3618a68ba13dda2
Aditya Sharma <sharmaadityamj@gmail.com>
2025-12-26 23:43:57 +0530

    Home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Component/Navbar';
 import Footer from './Component/Footer';
+import Home from './Pages/Home';
 
 
 
@@ -13,7 +14,10 @@ function App() {
 
       {/* Main content */}
       <main>
-        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
       </main>
 
       {/* Footer always visible */}
